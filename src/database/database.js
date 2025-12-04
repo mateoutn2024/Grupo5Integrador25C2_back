ad1120890f8204cc2e561b174d1e5eff2cfb9aa3
Mateo <mateoutn2024@gmail.com>
2025-12-04 12:06:58 -0300

    incorporando footer

--- src/database/database.js
+++ src/database/database.js
@@ -6,6 +6,6 @@ const { database } = environments;
 export const sequelize = new Sequelize(database.name, database.user, database.password, {
     host: database.host,
     dialect: 'mysql', // Usamos MySQL
-    port: 3307,       // CAMBIAR A 3307 SI TU XAMPP LO REQUIERE
+    port: 3306,       // CAMBIAR A 3307 SI TU XAMPP LO REQUIERE
     logging: false,
 });
